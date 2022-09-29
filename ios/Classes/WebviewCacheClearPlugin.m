95dd0e43a98cfbd0e2781c02dfbae7c2339e2fe5
赵远东 <1508574114@qq.com>
2022-09-29 10:36:28 +0800

    feat： ios代码加入测试，加入头文件，调整格式

--- ios/Classes/WebviewCacheClearPlugin.m
+++ ios/Classes/WebviewCacheClearPlugin.m
@@ -1,31 +1,32 @@
 #import "WebviewCacheClearPlugin.h"
+#import <WebKit/WebKit.h>
 
 @implementation WebviewCacheClearPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"webview_cache_clear"
-            binaryMessenger:[registrar messenger]];
-  WebviewCacheClearPlugin* instance = [[WebviewCacheClearPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"webview_cache_clear"
+                                     binaryMessenger:[registrar messenger]];
+    WebviewCacheClearPlugin* instance = [[WebviewCacheClearPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  }else  if ([@"clear" isEqualToString:call.method]) {
-                if (@available(iOS 9.0, *)) {
-                    NSSet *websiteDataTypes = [WKWebsiteDataStore allWebsiteDataTypes];
-                    NSDate *dateFrom = [NSDate dateWithTimeIntervalSince1970:0];
-                    [[WKWebsiteDataStore defaultDataStore] removeDataOfTypes:websiteDataTypes modifiedSince:dateFrom completionHandler:^{
-                    }];
-                } else {
-                    // Fallback on earlier versions
-                }
-              result(nil);
-}   else {
-    result(FlutterMethodNotImplemented);
-  }
-
+    if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    }else  if ([@"clear" isEqualToString:call.method]) {
+        if (@available(iOS 9.0, *)) {
+            NSSet *websiteDataTypes = [WKWebsiteDataStore allWebsiteDataTypes];
+            NSDate *dateFrom = [NSDate dateWithTimeIntervalSince1970:0];
+            [[WKWebsiteDataStore defaultDataStore] removeDataOfTypes:websiteDataTypes modifiedSince:dateFrom completionHandler:^{
+            }];
+        } else {
+            // Fallback on earlier versions
+        }
+        result(nil);
+    }   else {
+        result(FlutterMethodNotImplemented);
+    }
+    
 }
 
 @end
